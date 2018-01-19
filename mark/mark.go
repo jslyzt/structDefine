e6158cec066f0a7827687e65abdab8cfaab6ed85
zhoutao <zhout@hiscene.com>
2018-01-19 17:15:10 +0800

    mark库更新

--- mark/mark.go
+++ mark/mark.go
@@ -6,13 +6,50 @@ type BitMark struct {
 }
 
 // Set 设置标记
-func (mark *BitMark) Set(id uint32, set bool) {
+func (mark *BitMark) Set(id uint32, bset bool) {
+	if id <= 0 {
+		return
+	}
+	id--
+	findex := int(id) / 8
+	sindex := uint8(id % 8)
+	ldata := len(mark.data)
+	if findex+1 > ldata {
+		if bset == false {
+			return
+		}
+		ndata := make([]uint8, findex+1)
+		for index := 0; index < ldata; index++ {
+			ndata[index] = mark.data[index]
+		}
+		mark.data = ndata
+	}
+	mark.set(&mark.data[findex], sindex, bset)
+}
 
+func (mark *BitMark) set(data *uint8, index uint8, bset bool) {
+	if data == nil || index >= 8 {
+		return
+	}
+	if bset == true {
+		*data |= uint8(1 << index)
+	} else {
+		*data ^= uint8(1 << index)
+	}
 }
 
 // Get 获取标记
 func (mark *BitMark) Get(id uint32) bool {
-	return false
+	if id <= 0 || mark.data == nil {
+		return false
+	}
+	id--
+	findex := int(id) / 8
+	if findex >= len(mark.data) {
+		return false
+	}
+	sindex := uint8(id % 8)
+	return (mark.data[findex] & uint8(1<<sindex)) != 0
 }
 
 // Is 是否标记
